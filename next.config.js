04b4cd0a05d1d6ca2f522b1c87e8ed3da0e55885
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-08-14 09:41:03 +1000

    fddf

--- next.config.js
+++ next.config.js
@@ -3,15 +3,8 @@ const nextConfig = {
 	reactStrictMode: false,
 	images: {
 		domains: ['assets.vercel.com', 'kinopoiskapiunofficial.tech']
-	},
-	async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: 'https://kinopoiskapiunofficial.tech/:path*'
-			}
-		]
 	}
 }
+
 
 module.exports = nextConfig
