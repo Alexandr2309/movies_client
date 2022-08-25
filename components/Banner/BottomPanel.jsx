31179de7fabde162063290ca47a258d0cea11dc3
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-08-25 10:58:50 +1000

    final local version

--- components/Banner/BottomPanel.jsx
+++ components/Banner/BottomPanel.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Stars from '../UI/Stars/Stars'
 import cl from '../../styles/BannerBottom.module.scss'
 import Image from 'next/image'
+import likes from '../../public/img/like.png'
+import comments from '../../public/img/comments.png'
 
 const BottomPanel = ({ info }) => {
 	return (
@@ -14,12 +16,12 @@ const BottomPanel = ({ info }) => {
 			</div>
 			<div className={cl.right}>
 				<div className='likes'>
-					<Image src='/../public/like.png' alt='likes' width={14} height={14} />
+					<Image src={likes} alt='likes' width={14} height={14} />
 					<span>{info.ratingKinopoiskVoteCount}</span>
 				</div>
 				<div className={cl.com}>
 					<Image
-						src='/../public/comments.png'
+						src={comments}
 						alt='comments'
 						width={14}
 						height={14}
